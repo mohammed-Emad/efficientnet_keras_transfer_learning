1b3e341e2738d84be615a29b3fd6a50f62609076
mohammed-Emad <mohammed.imad38@yahoo.com>
2021-01-25 05:14:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'efficientnet'
+NAME = 'efficientnet2'
 DESCRIPTION = 'EfficientNet model re-implementation. Keras.'
 URL = 'https://github.com/qubvel/efficientnet'
 EMAIL = 'qubvel@gmail.com'
